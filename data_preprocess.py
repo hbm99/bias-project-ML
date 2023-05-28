0f7629348e89b60b77bf3972632b8846e1794bcd
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-05-28 12:54:30 -0400

    Add `hard_debiasing` initial methods.

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,10 +4,43 @@ import random as rd
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
+RACE_MAPPER = {0:'white', 1:'black',2: 'asian',3: 'indian', 4:'other'}
+GENDER_MAPPER = {0:'male',1:'female'}
 
-def data_selection(ds_path: str = 'datasets/utkface', k: int = 5):
+
+def data_selection(ds_path: str = 'data/utkface', k: int = 5):
+    
+    df = load_dataset(ds_path)
+    
+    df = map_values(df)
+    
+    # Stratified KFold
+    skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=1)
+
+    X = df['filepath']
+    y = df['gender'] + df['race']
+    r = rd.randint(0, k - 1)
+    train_idx = []
+    test_idx = []
+
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        if i == r:
+            train_idx=train_index
+            test_idx=test_index
+            break
+
+    train_data = df.iloc[train_idx]
+    test_data = df.iloc[test_idx]
+
+    return train_data, test_data
+
+def load_dataset(ds_path: str = 'data/utkface'):
     # Loading filenames
-    for dirname, _, filenames in os.walk(ds_path):
+    filenames = os.listdir(ds_path)
+    
+    try:
+        filenames.remove('.DS_Store')
+    except:
         pass
     
     # Building the dataframe
@@ -16,41 +49,13 @@ def data_selection(ds_path: str = 'datasets/utkface', k: int = 5):
     df['age'] = df.filename.apply(lambda x: int(x.split('_')[0]))
     df['gender'] = df.filename.apply(lambda x: int(x.split('_')[1]))
     df['race'] = df.filename.apply(lambda x: int(x.split('_')[-2]))
-
-    race_mapper = {0:'white', 1:'black',2: 'asian',3: 'indian', 4:'other'}
-    gender_mapper = {0:'male',1:'female'}
     
-    #Preprocess
+    return df
+
+def map_values(df: pd.DataFrame):
     for i in range(len(df)):
-        #img = np.array(Image.open(data_x[i]).resize((IMG_WIDTH,IMG_HEIGHT))) / 255.
-        #images.append(img)
-
-        #df['age']= df.age.apply(lambda x: x/116.)
-        df['gender'][i]= gender_mapper[df['gender'][i]]
-        df['race'][i]= race_mapper[df['race'][i]]
+        df['gender'][i]= GENDER_MAPPER[df['gender'][i]]
+        df['race'][i]= RACE_MAPPER[df['race'][i]]
+    return df
     
 
-    #KFold
-    skf=StratifiedKFold(n_splits=k, shuffle=True, random_state=1)
-
-    X=df['filepath']
-    y=df['gender']+ df['race']
-    r= rd.randint(0,k-1)
-    train_idx=[]
-    test_idx=[]
-
-    for i,(train_index, test_index) in enumerate(skf.split(X, y)):
-        x_train_fold, x_test_fold = X[train_index], X[test_index]
-        y_train_fold, y_test_fold = y[train_index], y[test_index]
-        # print(f"Fold {i}:")
-        # print(f"  Train: index={train_index}")
-        # print(f"  Test:  index={test_index}")
-        if(i==r):
-            train_idx=train_index
-            test_idx=test_index
-            break
-
-    train_data= df.iloc[train_idx]
-    test_data= df.iloc[test_idx]
-
-    return train_data, test_data
--- debiased_clip/hard_debiased/clip_hard_debiasing.py
+++ debiased_clip/hard_debiased/clip_hard_debiasing.py
@@ -1,9 +1,53 @@
+from __future__ import annotations
 
 import sys
+from typing import List
+
+import torch
 
 sys.path.append('/Users/hanselblanco/Documents/4to/ML/project/bias-project-ML')
-from data.md_gender.md_gender import get_md_gender
+import clip
+import pandas as pd
+from PIL import Image
 
-df = get_md_gender('image_chat')
+from data.md_gender.md_gender import get_md_gender
+from data_preprocess import data_selection
+
+R_COUNT = 1000
+CLIP_MODEL, CLIP_PREPROCESS = clip.load("ViT-B/32", "cuda" if torch.cuda.is_available() else "cpu")
+
+def get_data_to_encode():
+    # Load dataframes
+    df_pictures, df_test_pictures = data_selection()
+    df_text = get_md_gender('image_chat')
+
+    df_pictures_males = df_pictures.loc[df_pictures['gender'] == 'male'].sample(R_COUNT)
+    df_pictures_females = df_pictures.loc[df_pictures['gender'] == 'female'].sample(R_COUNT)
+
+    df_text_males = df_text.loc[df_text['male'] and not df_text['female']].sample(R_COUNT)
+    df_text_females = df_text.loc[df_text['female'] and not df_text['male']].sample(R_COUNT)
+    
+    return [df_pictures_males, df_pictures_females], [df_text_males, df_text_females]
+
+def encode(pic_dataframes: List[pd.Dataframe], text_dataframes: List[pd.Dataframe]):
+    for df in pic_dataframes:
+        df['image_tensor'] = df.photo_path.apply(lambda x: torch.tensor(CLIP_PREPROCESS(Image.open(x))))
+        df['image_encoded'] = df.image_tensor.apply(lambda x: CLIP_MODEL.encode_image(x))
+    for df in text_dataframes:
+        df['text_encoded'] = df.text.apply(lambda x: CLIP_MODEL.encode_text(x))
+    return pic_dataframes, text_dataframes
+
+def get_gender_subspace(R_male: pd.Series, R_female: pd.Series):
+    pass
+
+if __name__ == '__main__':
+    dfs_pics, dfs_text = encode(get_data_to_encode())
+    R_male = dfs_pics[0]['image_encoded'].append(dfs_text[0]['text_encoded'], ignore_index=True)
+    R_female = dfs_pics[1]['image_encoded'].append(dfs_text[1]['text_encoded'], ignore_index=True)
+    V = get_gender_subspace(R_male, R_female)
+    
+    
+    
+    
 
 
